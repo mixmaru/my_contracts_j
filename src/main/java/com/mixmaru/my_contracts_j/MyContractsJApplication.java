fa8d35c57c4a02e6dc1773653134fa07a13b0752
mixmaru <mix.touch777@gmail.com>
2021-12-09 20:42:16 +0900

    spring bootの初期設定

---
+++ src/main/java/com/mixmaru/my_contracts_j/MyContractsJApplication.java
@@ -0,0 +1,13 @@
+package com.mixmaru.my_contracts_j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyContractsJApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyContractsJApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mixmaru/my_contracts_j/MyContractsJApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mixmaru.my_contracts_j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyContractsJApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
