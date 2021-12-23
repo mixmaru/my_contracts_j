bf6bf33ccea1383c7598c9d498f6c7fb2df2f390
mixmaru <mix.touch777@gmail.com>
2021-12-23 22:46:46 +0900

    UserMapperでlombok使うのやめた

--- src/main/java/com/mixmaru/my_contracts_j/infra/user/mapper/UserMapper.java
+++ src/main/java/com/mixmaru/my_contracts_j/infra/user/mapper/UserMapper.java
@@ -1,20 +1,38 @@
 package com.mixmaru.my_contracts_j.infra.user.mapper;
 
-import lombok.Data;
-
 import javax.persistence.*;
 import java.time.ZonedDateTime;
 
-@Data
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
 @Table(name = "users")
-public class UserMapper {
+public abstract class UserMapper {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     private ZonedDateTime createdAt;
+    public ZonedDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(ZonedDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
     private ZonedDateTime updatedAt;
+    public ZonedDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+    public void setUpdatedAt(ZonedDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
 
     public UserMapper() {}
 
